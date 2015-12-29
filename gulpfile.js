58805bc4ab72250c3a7fad41bf9446291fcd55d7
Stephen Way <way.stephen@gmail.com>
2015-12-29 15:10:03 -0800

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
-var simplevars = require('postcss-simple-vars');
-var cssimport = require('postcss-import');
-var nested = require('postcss-nested');
-var mixins = require('postcss-mixins');
-var extend = require('postcss-extend');
+var postcssImport = require('postcss-import');
+var postcssNested = require('postcss-nested');
+var postcssMixins = require('postcss-mixins');
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
 
-// Compile css into /example
-gulp.task('css', function() {
-        var processors = [
-                cssimport,
-                autoprefixer(['last 2 versions']),
-                mixins,
-                simplevars,
-                nested,
-                extend
-        ];
-        return gulp.src('example/index.css')
-                .pipe(postcss(processors))
-                .pipe(gulp.dest('example/build'));
+// Check code style of stylesheets
+gulp.task('css:lint', function() {
+  return gulp.src('index.css')
+    .pipe(postcss([
+      stylelint,
+      reporter({ clearMessages: true }),
+    ]));
 });
 
-gulp.task('watch', function() {
+// Compile css into /example
+gulp.task('css', ['css:lint'], function() {
+  var processors = [
+    postcssImport,
+    postcssMixins,
+    postcssNested,
+    autoprefixer(['last 2 versions'])
+  ];
+  return gulp.src('example/index.css')
+    .pipe(postcss(processors).on('error', gutil.log))
+    .pipe(gulp.dest('example/build'));
+});
 
-        // Watch css and compile upon file changed
-        gulp.watch('test.css', ['css']);
+// Watch css and compile upon file changed
+gulp.task('watch', function() {
+  gulp.watch('example/index.css', ['css']);
 });
 
